7248a9db0f9d036f8fb70afb9a20bcabcbdd0327
1256ef <47380782+1256ef@users.noreply.github.com>
2019-02-06 14:57:34 +0530

    Create deepika.java

---
+++ deepika.java
@@ -0,0 +1,29 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int a;
+		Scanner S=new Scanner(System.in);
+		System.out.println("");
+		a=S.nextInt();
+		if(a==0)
+		{
+			System.out.println("Zero");
+		}
+		else if(a>0)
+		{
+			System.out.println("Positive");
+		}
+		else
+		{
+			System.out.println("Negative");
+		}
+	}
+}
